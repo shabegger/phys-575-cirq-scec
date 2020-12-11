9b78a16debbec0aaea18549cda4dbf090e469d75
Stephen Habegger <shabegger@users.noreply.github.com>
2020-12-10 20:37:52 -0800

    Stabilizer circuit

--- README.md
+++ README.md
@@ -8,8 +8,16 @@ University of Washington - Autumn 2020
 
 ## Installation
 
-Follow the instructions [here](https://cirq.readthedocs.io/en/stable/docs/install.html) to install Cirq.
+To install the required packages, run
+
+```bash
+$ pip install -r requirements.txt
+```
 
 ## References
 
 Fowler, Austin G, Mariantoni, Matteo, Martinis, John M, & Cleland, Andrew N. (2012). Surface codes: Towards practical large-scale quantum computation. Physical Review. A, Atomic, Molecular, and Optical Physics, 86(3), Physical review. A, Atomic, molecular, and optical physics, 2012-09-18, Vol.86 (3).
+
+Roffe, J. (2019). Quantum error correction: An introductory guide. Contemporary Physics, 60(3), 226-245.
+
+Criger, Ben, & Ashraf, Imran. (2018). Multi-path Summation for Decoding 2D Topological Codes. Quantum, 2, 102.
--- main.py
+++ main.py
@@ -1,17 +1,135 @@
 import cirq
-import deutsch
 
-sim = cirq.Simulator()
+from math import ceil, floor
+from more_itertools import roundrobin
 
-unitaries = [
-    deutsch.Constant0, 
-    deutsch.Constant1,
-    deutsch.BalancedI,
-    deutsch.BalancedX,
-]
+D = 3
 
-for unitary in unitaries:
-    circuit = deutsch.create(unitary, 0.05)
-    result = sim.run(circuit, repetitions=100)
-    print(circuit)
-    print(result.histogram(key='result'))
+size = 2 * D + 1
+
+controlSuffix = 1
+def nextControlQubitName():
+    global controlSuffix
+
+    name = 'Control{}'.format(controlSuffix)
+    controlSuffix = controlSuffix + 1
+
+    return name
+
+qubits = [[None for i in range(size)] for j in range(size)]
+for i in range(1, size - 1):
+    for j in range(1, size - 1):
+        if ((floor(i / 2) + ceil(j / 2) > (D - 1) / 2) and
+            (ceil(j / 2) - ceil(i / 2) < (D + 1) / 2) and
+            (floor(i / 2) - floor(j / 2) < (D + 1) / 2) and
+            (ceil(i / 2) + floor(j / 2) < (3 * D + 1) / 2)):
+            qubits[j][i] = cirq.GridQubit(i, j)
+
+def translateDataQubitCoords(x, y):
+    return (x + y + 1, D + y - x)
+
+def translateMeasureQubitCoords(x, y):
+    return (x + y, D + y - x)
+
+def stabilizeZ(x, y, for_print = False):
+    if ((x + y) % 2 == 1):
+        raise Exception('({}, {}) is not a valid Z measure qubit'.format(x, y))
+
+    translated = translateMeasureQubitCoords(x, y)
+    i = translated[0]
+    j = translated[1]
+
+    measureQubit = qubits[j][i]
+    if measureQubit is None:
+        return
+
+    yield cirq.I(measureQubit)
+
+    dataQubit = qubits[j][i - 1]
+    if dataQubit is None:
+        yield cirq.I(measureQubit)
+    else:
+        yield cirq.CNOT(dataQubit, measureQubit)
+    
+    dataQubit = qubits[j - 1][i]
+    if dataQubit is None:
+        yield cirq.I(measureQubit)
+    else:
+        yield cirq.CNOT(dataQubit, measureQubit)
+
+    dataQubit = qubits[j + 1][i]
+    if dataQubit is None:
+        yield cirq.I(measureQubit)
+    else:
+        yield cirq.CNOT(dataQubit, measureQubit)
+
+    dataQubit = qubits[j][i + 1]
+    if dataQubit is None:
+        yield cirq.I(measureQubit)
+    else:
+        yield cirq.CNOT(dataQubit, measureQubit)
+
+    yield cirq.I(measureQubit)
+    yield cirq.measure(measureQubit)
+
+    if not for_print:
+        controlQubit = cirq.NamedQubit(nextControlQubitName())
+        yield cirq.SWAP(measureQubit, controlQubit)
+
+def stabilizeX(x, y, for_print = False):
+    if ((x + y) % 2 == 0):
+        raise Exception('({}, {}) is not a valid X measure qubit'.format(x, y))
+
+    translated = translateMeasureQubitCoords(x, y)
+    i = translated[0]
+    j = translated[1]
+
+    measureQubit = qubits[j][i]
+    if measureQubit is None:
+        return
+
+    yield cirq.H(measureQubit)
+
+    dataQubit = qubits[j][i - 1]
+    if dataQubit is None:
+        yield cirq.I(measureQubit)
+    else:
+        yield cirq.CNOT(measureQubit, dataQubit)
+    
+    dataQubit = qubits[j + 1][i]
+    if dataQubit is None:
+        yield cirq.I(measureQubit)
+    else:
+        yield cirq.CNOT(measureQubit, dataQubit)
+
+    dataQubit = qubits[j - 1][i]
+    if dataQubit is None:
+        yield cirq.I(measureQubit)
+    else:
+        yield cirq.CNOT(measureQubit, dataQubit)
+
+    dataQubit = qubits[j][i + 1]
+    if dataQubit is None:
+        yield cirq.I(measureQubit)
+    else:
+        yield cirq.CNOT(measureQubit, dataQubit)
+
+    yield cirq.H(measureQubit)
+    yield cirq.measure(measureQubit)
+
+    if not for_print:
+        controlQubit = cirq.NamedQubit(nextControlQubitName())
+        yield cirq.SWAP(measureQubit, controlQubit)
+
+operations = []
+for y in range(D + 1):
+    for x in range(D + 1):
+        if ((x + y) % 2 == 0):
+            operations.append(stabilizeZ(x, y, True))
+        else:
+            operations.append(stabilizeX(x, y, True))
+
+circuit = cirq.Circuit()
+circuit.append(roundrobin(*operations))
+
+print(circuit)
